793facbb859b69e58c09ec0598e9bd5ab99692e5
Michal Vich <vich@zensys.cz>
2014-07-24 12:57:06 +0200

    init version

---
+++ slider.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    Bootstrap.BootStrapSlider = Ember.TextField.extend({
+
+        attributeBindings: ['max', 'value'],
+
+        updateValue: function() {
+            this.$().slider({
+                max: this.get('max'),
+                value: this.get('value'),
+                tooltip: 'hide'
+            });
+        }.observes('value'),
+
+        didInsertElement: function() {
+            var self = this;
+            this.$().slider({
+                max: this.get('max'),
+                value: this.get('value'),
+                tooltip: 'hide'
+            });
+
+            this.$().on('slide', function() {
+                self.set('value', this.value);
+            });
+
+            this.$().on('slideStop', function() {
+                self.set('value', this.value);
+            });
+
+        },
+
+        willDestroyElement: function(){
+            this.$().off('slide');
+            this.$().off('slideStop');
+        }
+
+    });
+
+})();
